4643a340cedffbc8a2cf3672aca06c330978ef73
aishu16191-boop <aishu16191@gmail.com>
2026-05-07 09:20:41 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
-import { addFaculty } from './modules/handlers.js';
-
-document.getElementById("addBtn")
-.addEventListener("click", addFaculty);
+import { state, subscribe } from "./state.js"; 
+import { render } from "./renderer.js"; 
+import { EmployeeList } from "./components/EmployeeList.js";
+import "./components/Controls.js";
+const root = document.getElementById("app"); 
+function App() { return ` 
+    ${EmployeeList(state.employees)} 
+    `;
+ }
+  function updateUI() 
+  { render(App(), root); } 
+  subscribe(updateUI); 
+  updateUI();
